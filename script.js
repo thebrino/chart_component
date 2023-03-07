c4ca070e32ba097a94d9256f3818b9414aaff90a
thebrino <bm13141455@gmail.com>
2023-03-07 16:33:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//youtu.be/eS-FVnhjvEQ
+
+fetch("data.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (products) {
+    let placeholder = document.querySelector("#data-output");
+  });
+
+console.log("a");
